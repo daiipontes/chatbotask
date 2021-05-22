956305c9baac383146e21c14bdfe220b0eba4aeb
daiipontes <83508006+daiipontes@users.noreply.github.com>
2021-05-22 13:25:57 -0300

    Update ChatbotPython.py

--- ChatbotPython.py
+++ ChatbotPython.py
@@ -3,11 +3,11 @@ import sys
 def processar_resposta(resposta):
  
   if resposta == '1':
-        print ('No nosso site de sáude você pode acompanhar todas as noticias do por você e de todo o calendario de saúde, além disso pode se inscrever no nosso programa de acompanhamento nutricional\nLink do site')
+        print ('No nosso site de sáude você pode acompanhar todas as noticias do por você e de todo o calendário de saúde, além disso pode se inscrever no nosso programa de acompanhamento nutricional\nLink do site')
   elif resposta == '2': 
         print ('\nEm casos de sintomas ligar para os telefones (11)xxxxx-xxxx ou (11)xxxxx-xxxx para ter o atendimento individual, eles funcionam 24 horas por dia e os atendimentos são feitos exclusivamente via ligação.\n')
   elif resposta == '3': 
-        print ('\nOs ramais disponíveis no ambulatorio são:\n xxxx para reagendamentos e dúvidas\n xxxx para acidentes e emergencias.\n')
+        print ('\nOs ramais disponíveis no ambulatorio são:\n xxxx para reagendamentos e dúvidas\n xxxx para acidentes e emergências.\n')
   elif resposta == 'Sair': 
         print ('\nAgradeçemos o seu contato.\n')
         sys.exit()              
@@ -22,13 +22,13 @@ def start():
 
 #matricula do funcionário substituia o nome 
 
-  nome = input('Digite seu nome')
-  email =  input ('digite seu email')
+  nome = input('Digite seu nome ')
+  email =  input ('digite seu email ')
 
  
   while True:
  
-      resposta = input (f'\nObrigada, agorara é só escolher uma das opções abaixo\nLembrando que para encerrar o seu atendimento é só nos mandar a palavrada:Sair\n1- Site de saúde;\n2- Informações de contato é Corona;\n3- Ramal Ambulatório\n')
+      resposta = input (f'\nAgora é só escolher uma das opções abaixo\nLembrando que para encerrar o seu atendimento é só nos mandar a palavra: Sair\n1- Site de saúde;\n2- Informações de contato é Corona;\n3- Ramal Ambulatório;\n')
  
       processar_resposta(resposta)
 
